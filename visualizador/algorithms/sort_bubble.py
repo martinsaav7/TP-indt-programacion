efeda42437ff2dc532d907072fda2b457e674c27
Bautista Torres <141366607+Bautista-Leonel-Torres@users.noreply.github.com>
2025-11-05 10:33:04 -0300

    resuelto burbuja

--- visualizador/algorithms/sort_bubble.py
+++ visualizador/algorithms/sort_bubble.py
@@ -11,9 +11,20 @@ def init(vals):
     n = len(items)
     i = 0
     j = 0
-fdsdfsdfsdasdasdas
+
 def step():
-    # TODO:
+    global items,n,i,j
+    if i>=n-1:
+        return{"done": True}
+    swap=False
+    if items[j]>items[j+1]:
+        j,j+1=j+1,j
+        swap= True
+    if j>= n-i-1:
+        j=0
+        i+=1
+    return {"j":j, "i":i, "swap":swap, "done":False}
+    
     # 1) Elegir índices a y b a comparar en este micro-paso (según tu Bubble).
     # 2) Si corresponde, hacer el intercambio real en items[a], items[b] y marcar swap=True.
     # 3) Avanzar punteros (preparar el próximo paso).
